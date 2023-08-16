876ad1a89cca0998d3ab5056a195e5dc41f83a88
Yusup <hunzah9@mail.ru>
2023-08-16 21:05:10 +0300

    commit

--- src/app/components/main/orderinfo/OrderInfo.tsx
+++ src/app/components/main/orderinfo/OrderInfo.tsx
@@ -1,6 +1,6 @@
-import styles from '@/app/components/main/order/order.module.css';
+import styles from '@/app/components/main/orderinfo/orderInfo.module.css';
 
-export const Order = () => {
+export const OrderInfo = () => {
     return (
         <div className={styles.container}>
             <div className={styles.date}>
@@ -9,8 +9,10 @@ export const Order = () => {
             <div className={styles.info}>
                 <span className={styles.distance}></span>
                 <img className={styles.img}/>
-                <span className={styles.stoneSize}></span>
+                <span className={styles.text}><div className={styles.stoneSize} ></div></span>
             </div>
+
+            <Order/>
 
         </div>
     );
---
+++ src/app/components/main/orderinfo/order/Order.tsx
@@ -0,0 +1,10 @@
+import styles from '@/app/components/main/orderinfo/order/order.module.css';
+
+export const OrderInfo = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+};
+
