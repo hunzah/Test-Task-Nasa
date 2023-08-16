8804c8be36cbdc7151f3041bf6a1ec0cbaac442d
Yusup <hunzah9@mail.ru>
2023-08-16 20:04:18 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import styles from './styles/home.module.css'
 
 
+
 export default function Home() {
     return (
         <>
             <div className={styles.container}>
-                <divc >
+                <div className={styles.titleContainer} >
                     <h1 className={styles.title}>ARMAGEDDON 2023</h1>
-                    <div>ООО “Команда им. Б. Уиллиса”. Взрываем астероиды с 1998 года.</div>
-                </divc>
+                    <div>ООО “Команда им. Б. Уиллиса”.</div>
+                    <div>Взрываем астероиды с 1998 года.</div>
+                </div>
+                <Main/>
                 <div className={styles.bgColor}>
                 </div>
             </div>
