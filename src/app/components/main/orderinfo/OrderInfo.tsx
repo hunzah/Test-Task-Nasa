34518f78fa4b253d48fef18ac340d4f7a75cf66b
Yusup <hunzah9@mail.ru>
2023-08-18 14:06:32 +0300

    commit

--- src/app/components/main/orderinfo/OrderInfo.tsx
+++ src/app/components/main/orderinfo/OrderInfo.tsx
@@ -2,9 +2,15 @@ import styles from '@/app/components/main/orderinfo/orderInfo.module.css';
 import Button from '@/app/assets/Button';
 import {getAsteroidsData} from '@/app/api/nasaApi';
 import {formatDate} from '@/app/utils/formatDate';
-import AsteroidImg from './../../../../../public/stone.png'
+
+
 
 export const OrderInfo = async () => {
+
+    // const [lunar,setLunar] = useState(false)
+    // const [km,setKM] = useState(true)
+
+
     const currentDate = new Date();
     const endDate = new Date(currentDate);
     endDate.setDate(endDate.getDate());
@@ -33,27 +39,33 @@ export const OrderInfo = async () => {
                         className={styles.date}> {formatDate(asteroid.close_approach_data[0].close_approach_date)}
                     </span>
 
-                    {asteroid.is_potentially_hazardous_asteroid[0] ?
-                        <img className={styles.bigAsteroidImg} src={'/stone.png'} alt={asteroid.name}/>
-                        :
-                        <img className={styles.smallAsteroidImg} src={'/stone.png'} alt={asteroid.name}/>
-                    }
 
-                    <span className={styles.distance}>
-                        {lunarOrbits} {lunarOrbitsText}
-                    </span>
-
-                    {/*<div className={styles.stoneSize}>*/}
-                    {/*    {Number(asteroid.close_approach_data[0].miss_distance.lunar).toFixed(2)} FQ*/}
-                    {/*</div>*/}
-                    <div className={styles.stoneSize}>
-                        {Number(asteroid.estimated_diameter.meters.estimated_diameter_min).toFixed(2)} м
+                    <div className={styles.infoContainer}>
+                        <div className={styles.distanceContainer}>
+                        <span className={styles.distance}>
+                            {lunarOrbits} {lunarOrbitsText}
+                        </span>
+                            <img src="/Arrow.svg" className={styles.arrowImg}/>
+                        </div>
+                        {asteroid.is_potentially_hazardous_asteroid[0] ?
+                            <img className={styles.bigAsteroidImg} src={'/stone.png'} alt={asteroid.name}/>
+                            :
+                            <img className={styles.smallAsteroidImg} src={'/stone.png'} alt={asteroid.name}/>
+                            }
+                        <div className={styles.diamContainer}>
+                            <span className={styles.fq}> 2021 FQ </span>
+                            <span className={styles.stoneSize}>
+                               Ø {Number(asteroid.estimated_diameter.meters.estimated_diameter_min).toFixed(2)} м
+                            </span>
+                        </div>
                     </div>
+
+
                     <Button text="заказать"/>
                     {asteroid.is_potentially_hazardous_asteroid ? <span>⚠️Опасен</span> : <span>🕊️ Безобидный</span>}
 
 
-                </>
+               </>
             );
             asteroidItems.push(asteroidMarkup);
         });
